4a07da195ff05c87a20f8b22d19016f898ed1878
Ayushi Srivastava <ayushimanoj1201@gmail.com>
2022-04-26 20:14:15 +0530

    style : added css

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from 'react';
+import React,{useState} from 'react';
 import { useTable ,useGlobalFilter,useFilters, useRowSelect,usePagination} from "react-table";
 import { Checkbox } from './Checkbox';
 
